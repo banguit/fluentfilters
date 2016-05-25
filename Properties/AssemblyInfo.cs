631eaf09bd14b9b27b0f12b8218aadafa1234755
Dmytro Antonenko <dmitry.antonenko@hedgehog.com.ua>
2016-05-25 02:26:37 -0700

    Updates of AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,14 +1,32 @@
-﻿using System.Reflection;
+﻿#region License
+// Copyright (c) Dmitry Antonenko (http://hystrix.com.ua)
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+// 
+// The latest version of this file can be found at http://fluentfilters.codeplex.com/
+#endregion
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FluentFilter")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("FluentFilters")]
+[assembly: AssemblyDescription("Library for ASP.NET Core, that will add support of criterias for global action filters.")]
+[assembly: AssemblyProduct("FluentFilters")]
+[assembly: AssemblyCopyright("Copyright (c) Dmytro Antonenko Antonenko 2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
